c640ae8d1e29aed39479c99f112d477d8d459e3f
Pierre-Yves Aillet <pyaillet@gmail.com>
2020-12-14 08:39:14 +0100

    :sparkles: day 14 solution

---
+++ 14/puzzle1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import sys 
+
+inp = 'test.txt'
+
+if len(sys.argv) == 2:
+    inp = sys.argv[1]
+
+class computer:
+    def __init__(self, instructions):
+        self.mem = {}
+        self.mask = ''
+        self.cur = 0
+        self.instructions = instructions
+
+    def set_mask(self, val):
+        self.mask = val
+
+    def set_mem(self, val, addr):
+        val36 = "{0:036b}".format(val)
+        new_val = ''
+        for i in range(0,36):
+            if self.mask[i] == 'X':
+                new_val += val36[i]
+            else:
+                new_val += self.mask[i]
+
+        self.mem[addr] = int(new_val, 2)
+
+    def run_step(self, ip):
+        cur_inst = self.instructions[ip]
+        if cur_inst[0] == 'mask':
+            self.set_mask(cur_inst[1])
+        elif cur_inst[0] == 'mem':
+            self.set_mem(cur_inst[1], cur_inst[2])
+
+    def run(self):
+        for i in range(0, len(self.instructions)):
+            self.run_step(i)
+
+    def sum_mem(self):
+        return sum([self.mem[k] for k in self.mem])
+
+
+def parse_inst(s):
+    arr = s.strip().split(' = ')
+    if arr[0] == 'mask':
+        return ('mask', arr[1])
+    elif arr[0][0:3] == 'mem':
+        return ('mem', int(arr[1]), arr[0].split('[')[1].strip(']'))
+
+instructions = list(map(parse_inst, open(inp, 'r').readlines()))
+print(instructions)
+c = computer(instructions)
+c.run()
+print(c.sum_mem())
